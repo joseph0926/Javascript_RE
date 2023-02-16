e7e2ea5379df6db1dafbc6113815293809449af0
김영훈 <joseph0926@naver.com>
2023-02-16 10:56:01 +0900

    DOM_ch07_2023.02.16

--- ch07/app.js
+++ ch07/app.js
@@ -10,3 +10,28 @@ titleText = "pedri";
 
 console.log(titleText);
 // 결과 => pedri
+
+/* const section = document.querySelector("section");
+section.style.backgroundColor = "red"; */
+
+const section = document.querySelector("section");
+const btn = document.querySelector("button");
+
+section.className = "red-bg";
+
+btn.addEventListener("click", () => {
+  section.classList.toggle("invisible");
+});
+
+const itemLi = document.querySelectorAll("li");
+console.log(itemLi);
+
+const itemLi2 = document.getElementsByTagName("li");
+console.log(itemLi2);
+
+const ul = document.querySelector("ul");
+const newLi = document.createElement("li");
+ul.appendChild(newLi);
+
+console.log(itemLi);
+console.log(itemLi2);
